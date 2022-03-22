4d2902a63a81db599fc38376f1429371a8a4345e
Giovani Franz <giovani.franz>
2022-03-22 19:12:11 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,31 @@
+import { Flex, Button, Stack } from '@chakra-ui/react'
+import { Input } from '../components'
+
+export default function SignIn() {
+  return (
+    <Flex
+      width="100vw"
+      height="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing={4}>
+          <Input name="nome" label="Nome" type="email" />
+          <Input name="email" label="Email" type="Senha" />
+        </Stack>
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  )
+}
